62ca3967907597aca6e0c2748dde57e869ee4d3e
Adam Thomas <thomasadam83@hotmail.com>
2025-11-19 10:42:29 -0500

    Cleaned up search page

--- applications/admin.dashboard.php
+++ applications/admin.dashboard.php
@@ -68,7 +68,7 @@ $applications_count = mysqli_num_rows($result);
     <?php while ($record = mysqli_fetch_assoc($result)): ?>
         <tr>
             <td>
-                <a href="<?=$record['url']?>" target="_blank"><?=$record['name']?></a>
+                <a href="/details/<?=$record['id']?>"><?=$record['name']?></a>
                 <br>
                 <small><?=string_shorten($record['description'], 100)?></small>
             </td>
@@ -99,64 +99,6 @@ $applications_count = mysqli_num_rows($result);
     <?php endwhile; ?>
 
 </table>
-
-<a
-    href="/admin/add"
-    class="w3-button w3-white w3-border"
->
-    <i class="fa-solid fa-pen-to-square fa-padding-right"></i> Add Application
-</a>
-
-
-<!--
-<a
-    href="/admin/import"
-    class="w3-button w3-white w3-border"
->
-    <i class="fa-solid fa-download"></i> Import Colours
-</a>
-
-<hr />
-
-<div
-    class="w3-row-padding"
-    style="margin-left: -16px; margin-right: -16px"
->
-    <div class="w3-half">
-        <div class="w3-card">
-            <header class="w3-container w3-grey w3-padding w3-text-white">
-                <i class="bm-colours"></i> Uptime Status
-            </header>
-            <div class="w3-container w3-padding">Uptime Status Summary</div>
-            <footer class="w3-container w3-border-top w3-padding">
-                <a
-                    href="/admin/uptime/colours"
-                    class="w3-button w3-border w3-white"
-                >
-                    <i class="fa-regular fa-file-lines fa-padding-right"></i>
-                    Full Report
-                </a>
-            </footer>
-        </div>
-    </div>
-    <div class="w3-half">
-        <div class="w3-card">
-            <header class="w3-container w3-grey w3-padding w3-text-white">
-                <i class="bm-colours"></i> Stat Summary
-            </header>
-            <div class="w3-container w3-padding">App Statistics Summary</div>
-            <footer class="w3-container w3-border-top w3-padding">
-                <a
-                    href="/stats/colours"
-                    class="w3-button w3-border w3-white"
-                >
-                    <i class="fa-regular fa-chart-bar fa-padding-right"></i> Full Report
-                </a>
-            </footer>
-        </div>
-    </div>
-</div>
--->
 
 <?php
 
